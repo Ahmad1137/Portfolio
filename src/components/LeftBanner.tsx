51a34c523cb733a9ccb18f0dd54b01271fb7b3fc
Ahmad Abdullah <135378526+Ahmad1137@users.noreply.github.com>
2024-02-23 12:34:47 +0500

    picture

--- src/components/LeftBanner.tsx
+++ src/components/LeftBanner.tsx
@@ -12,7 +12,7 @@ const LeftBanner = () => {
     delaySpeed: 2000,
   });
   return (
-    <FadeIn className="w-full lgl:w-1/2 flex flex-col gap-20">
+    <FadeIn className="w-full lgl:w-1/2 flex flex-col gap-20 gap-14-lg">
       <div className="flex flex-col gap-5">
         <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
         <h1 className="text-6xl font-bold text-white">
