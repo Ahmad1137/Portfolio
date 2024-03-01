2a4cd06bbc5ac2365904c1c4483cbcb31a3378ec
Ahmad Abdullah <135378526+Ahmad1137@users.noreply.github.com>
2024-03-01 20:31:41 +0500

    education

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -11,10 +11,9 @@ const Education = () => {
       {/* part one */}
       <div>
         <div className="py-6 lgl:py-12 font-titleFont text-center flex flex-col gap-4">
-          <p className="text-lg text-white tracking-[4px]">2024</p>
           <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
         </div>
-        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        <div className="mt-4 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title="BSc in Computer Science"
             subTitle="Superior University  (2020 - 2024)"
