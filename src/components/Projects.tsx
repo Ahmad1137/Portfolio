ce6f808442932193203b7fbb0da47a3a857e752e
Ahmad Abdullah <135378526+Ahmad1137@users.noreply.github.com>
2024-07-30 20:13:36 +0500

    todo app detal

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -36,8 +36,7 @@ const Projects = () => {
           />
           <ProjectsCard
             title="Todo App"
-            des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+            des="Todo App complete yu can edit update delete and completion of employee task"
             src={projectThree}
           />
           <ProjectsCard
